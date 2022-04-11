b81bb447ca8becbab3bd552382d03b2a60474ade
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2022-04-11 09:48:42 +0200

    test sub components

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+  const props = defineProps({
+    type: {
+      type: String,
+      required: false,
+      default: 'info'
+    }
+  });
+</script>
+
+<template>
+  <button :class="type">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  .info {
+    color: blue;
+  }
+  .error {
+    color: red;
+  }
+  .truc {
+    color: brown;
+  }
+</style>
