67f24060cb52a18af9c414d3d217ef620ad85f05
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2022-04-11 09:36:41 +0200

    test sub components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,73 @@
 <script setup>
-  import {ref, computed, watchEffect} from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import { ref, watchEffect } from 'vue';
+  import BaseButton from './components/BaseButton.vue';
 
-  const x = ref(0);
+  const distSi = ref(0.123);
 
-const y = computed({
-  get: () => x.value * 2,
-  set: val => x.value = 10
-})
 
-const msg = computed(() => {
-  const msg = x.value > 30 ? 'il fait chaud' : '';
-  return msg;
-})
 
-setTimeout(() => {
-  x.value = 40;
-}, 3000)
+  function round(val) {
+    return Math.round(val);
+  }
+
+  function onBtnClick(evt) {
+    console.log(evt)
+  }
+
+  const msg = "Click me !"
+
+  const username = ref(null);
+
+
+
+  function onUsernameChange(evt) {
+    username.value = evt.target.value;
+  }
+
+  const password = ref(null);
+
+  watchEffect(() => {
+    console.log(username.value);
+    console.log(password.value);
+  })
+
+  const error = ref(false);
+
+   setTimeout(() => {
+     error.value = true;
+   }, 2000)
 </script>
 
 <template>
-  <hello-world></hello-world>
-  <v-menu></v-menu>
-  <p>kelvin: {{ x }} </p>
-  <p>celcius: {{ y }}</p>
+  <h1>Temperature converter</h1>
+  <div> {{ round(distSi) }} </div>
+  <input type="submit" :value="msg" @click="onBtnClick($event)">
 
-  <div>{{ msg }}</div>
+  <base-button type="error">Error</base-button>
 
-</template>
+  <base-button type="info">Info</base-button>
+
+  <base-button type="truc">Truc</base-button>
+
+
+  <!-- <input
+    type="text"
+    name="username"
+    placeholder="username"
+    :value="username"
+    @input="onUsernameChange($event)"
+  > -->
+    <input type="text" v-model="username">
+    <input type="password" v-model="password">
+    <div v-show="error">
+      Wrong username or password
+    </div>
+  </template>
 
 <style>
-  p {
-    color: red;
+  input {
+    display: block;
+    padding: 0.5rem;
+    margin: 1rem;
   }
 </style>
